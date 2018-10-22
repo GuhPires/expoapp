167e61ae7ecb489ebbcf85605a31ebdb2278e225
Gustavo <gustavo.goncalvespires@gmail.com>
2018-10-22 16:00:53 -0300

    Testing time

--- App.js
+++ App.js
@@ -5,16 +5,44 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      isTyping: false,
-      text: ''
+      arrivingTime: 0,
+      leavingTime: 0,
+      claculatedTime: 0
     }
   }
+
+  getTime(e){
+    e.preventDefault();
+    let from = 'arrivingTime';
+    if(this.state.arrivingTime){
+      from = 'leavingTime';
+    }
+    this.setState({
+      [from]: Date.now()
+    });
+  }
+
+  calculateTime(e){
+    e.preventDefault();
+    let diff = (this.state.leavingTime - this.state.arrivingTime);
+    let hours = parseInt(diff / 3600000);
+    console.log('Hours: ', hours);
+    let minutes = (diff - hours) / 60000;
+    console.log('Minutes: ', minutes);
+    let seconds = (diff - minutes) / 60;
+    console.log('Seconds: ', seconds);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={styles.input} placeholder="Type something..." onChangeText={(text) => this.setState({ isTyping: true, text})} onBlur={() => this.setState({ isTyping: false })} value={this.state.text}/>
-        <Text style={styles.text}>{ this.state.isTyping ? 'Typing...' : 'Clear' }</Text>
-        <Button style={styles.button} title="Clear input" onPress={() => this.setState({ text: '' })}/>
+        <Text style={styles.label}>Arrived at:</Text>
+        <Text style={styles.text}>{new Date(this.state.arrivingTime).toLocaleTimeString()}</Text>
+        <Text style={styles.label}>Left at:</Text>
+        <Text style={styles.text}>{new Date(this.state.leavingTime).toLocaleTimeString()}</Text>
+        <Button title="Get time" onPress={this.getTime.bind(this)} color={'red'} disabled={this.state.leavingTime ? true : false}/>
+        <Text style={styles.label}>Time worked:</Text>
+        <Text style={styles.text}>{}</Text>
       </View>
     );
   }
@@ -23,27 +51,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-  input: {
-    fontSize: 20,
-    color: 'red',
-    width: 200,
-    borderWidth: 2,
-    borderStyle: 'dashed',
-    borderColor: 'black'
+    alignItems: 'center'
   },
   text: {
-    padding: 10,
-    fontSize: 22,
-    color: 'gray'
+    borderWidth: 1,
+    textAlign: 'center',
+    width: '60%',
+    height: 24,
+    fontSize: 20
   },
-  button: {
-    width: 100,
-    height: 20,
-    padding: 20,
-    borderColor: 'black'
+  label: {
+    color: 'lightgray'
   }
 });
